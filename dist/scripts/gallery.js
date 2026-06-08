e6501ad7055d2408d3c1c97fd206e72bfff4a74c
dinosaurSoup <gorfmorp@gmail.com>
2026-06-08 17:21:42 +0200

    Gallery works... kind of?

---
+++ dist/scripts/gallery.js
@@ -0,0 +1,35 @@
+let galIndex = 0;
+galleryDisplay(galIndex);
+
+function moveImage(n) {
+    galleryDisplay(galIndex += n);
+}
+
+function currImg(n) {
+    galleryDisplay(galIndex = n);
+}
+
+function galleryDisplay(n) {
+    let i;
+    let gallery = document.getElementsByClassName("gal-slide");
+    let dots = document.getElementsByClassName("gal-dot");
+
+    if (n >= gallery.length) {
+        galIndex = 0;
+    }
+
+    if (n < 0) {
+        galIndex = gallery.length - 1;
+    }
+
+    for (i = 0; i < gallery.length; i++) {
+        gallery[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    gallery[galIndex].style.display = "block";
+    dots[galIndex].className += " active";
+} 
--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,8 @@ export default function (eleventyConfig) {
 	eleventyConfig.setInputDirectory('src');
 	eleventyConfig.setOutputDirectory('dist');
     eleventyConfig.addPassthroughCopy("src/styles/styles.css");
-	eleventyConfig.addPassthroughCopy('src/images');
+	eleventyConfig.addPassthroughCopy('src/images/*');
+	eleventyConfig.addPassthroughCopy('src/scripts/*');
 }
 
 export const config = {
